da1e276ae47c560a8575f515b3350bc01e7bf004
Timothy D Meadows II <timothy@timothymeadows.com>
2020-05-04 06:01:22 -0700

    Fix bug with V3HiddenInput not having a default for action.

--- reCAPTCHA.AspNetCore/Versions/RecaptchaV3HiddenInput.cs
+++ reCAPTCHA.AspNetCore/Versions/RecaptchaV3HiddenInput.cs
@@ -6,6 +6,6 @@ namespace reCAPTCHA.AspNetCore.Versions
 {
     public class RecaptchaV3HiddenInput : RecaptchaVersion
     {
-        public string Action { get; set; }
+        public string Action { get; set; } = "homepage";
     }
 }
